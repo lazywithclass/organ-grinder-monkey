d6f64e2388648143bb33fb4457ce30a9ce252602
lazywithclass <montecristoh@gmail.com>
2015-01-13 23:38:30 +0000

    first commit with some code

---
+++ README.md
@@ -0,0 +1,7 @@
+# organ-grinder-monkey
+Not as lethal as the Chaos Monkey, still a monkey nonetheless. If you feel confident
+about your infrastructure you might want to use [scimmia](https://github.com/lazywithclass/scimmia)
+
+Require and configure this module to shutdown ec2-instances, with your configuration but without notice.
+
+
---
+++ index.js
@@ -0,0 +1,33 @@
+var AWS = require('aws-sdk'), ec2 = new AWS.EC2();
+
+module.exports = function(timeout, tags, beforeHook) {
+  timeout = timeout || 60 * 60 * 1000;
+  beforeHook = beforeHook || function(cb) { cb(); };
+  
+  setTimeout(function() {
+    beforeHook(terminate.bind(null, tags));
+  }, timeout * Math.random());
+};
+
+function terminate(tags) {
+  var params = {
+    Filters: [
+      { Name: 'instance-state-name', Values: ['running'] },
+      { Name: 'tag-key', Values: tags.names },
+      { Name: 'tag-value', Values: tags.values }
+    ]
+  };
+  
+  ec2.describeInstances(params, function(error, data) {
+    if (error) return console.log(error); 
+
+    var instanceIds = data.Reservations.map(function(reservation) {
+      return reservation.Instances[0].InstanceId;
+    });
+
+    ec2.terminateInstances({ InstanceIds: instanceIds}, function(err, data) {
+      if (error) console.log(err);
+      console.log(data);
+    });
+  });
+}
