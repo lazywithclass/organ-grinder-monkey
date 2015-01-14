bc1610af3798f9178f2548e72b1da25c959551d7
Alberto Zaccagni <montecristoh@gmail.com>
2015-01-14 13:47:06 +0000

    added a proposal for terminate and changeTags functions

--- index.js
+++ index.js
@@ -1,35 +1,74 @@
 var AWS = require('aws-sdk'), ec2 = new AWS.EC2();
 
-module.exports = function(timeout, tags, beforeHook) {
+module.exports.changeTag = function(timeout, oldTag, newTags, beforeHook) {
   timeout = timeout || 60 * 60 * 1000;
   beforeHook = beforeHook || function(cb) { cb(); };
   
   setTimeout(function() {
-    beforeHook(terminate.bind(null, tags));
+    beforeHook(changeTag);
   }, timeout * Math.random());
-};
 
-function terminate(tags) {
-  var params = {
-    Filters: [
-      { Name: 'instance-state-name', Values: ['running'] },
-      { Name: 'tag-key', Values: tags.names },
-      { Name: 'tag-value', Values: tags.values }
-    ]
-  };
+  function changeTag() {
+    var params = {
+      Filters: [
+        { Name: 'instance-state-name', Values: ['running'] },
+        { Name: 'tag-key', Values: oldTag.names },
+        { Name: 'tag-value', Values: oldTag.values }
+      ]
+    };
+    
+    ec2.describeInstances(params, function(error, data) {
+      if (error) return console.log(error); 
+
+      var instanceIds = data.Reservations.map(function(reservation) {
+        return reservation.Instances[0].InstanceId;
+      });
+
+      var instanceId = instanceIds[Math.floor(Math.random() * instanceIds.length)];
+
+      var params = {
+        Resources: [ instanceId ],
+        Tags: newTags
+      };
+
+      ec2.createTags(params, function(err, data) {
+        if (error) console.log(err);
+        console.log(data);
+      });
+    });
+  }
+}; 
+
+module.exports.terminate = function(timeout, tags, beforeHook) {
+  timeout = timeout || 60 * 60 * 1000;
+  beforeHook = beforeHook || function(cb) { cb(); };
   
-  ec2.describeInstances(params, function(error, data) {
-    if (error) return console.log(error); 
+  setTimeout(function() {
+    beforeHook(terminate);
+  }, timeout * Math.random());
 
-    var instanceIds = data.Reservations.map(function(reservation) {
-      return reservation.Instances[0].InstanceId;
-    });
+  function terminate() {
+    var params = {
+      Filters: [
+        { Name: 'instance-state-name', Values: ['running'] },
+        { Name: 'tag-key', Values: tags.names },
+        { Name: 'tag-value', Values: tags.values }
+      ]
+    };
     
-    var instanceId = instanceIds[Math.floor(Math.random() * instanceIds.length)];
-    
-    ec2.terminateInstances({ InstanceIds: [ instanceId ] }, function(err, data) {
-      if (error) console.log(err);
-      console.log(data);
+    ec2.describeInstances(params, function(error, data) {
+      if (error) return console.log(error); 
+
+      var instanceIds = data.Reservations.map(function(reservation) {
+        return reservation.Instances[0].InstanceId;
+      });
+
+      var instanceId = instanceIds[Math.floor(Math.random() * instanceIds.length)];
+
+      ec2.terminateInstances({ InstanceIds: [ instanceId ] }, function(err, data) { 
+        if (error) console.log(err);
+        console.log(data);
+      });
     });
-  });
-}
+  }
+};
