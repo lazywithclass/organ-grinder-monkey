2ca7633a86b5d6f188575245eb0a1f1856321d46
lazywithclass <montecristoh@gmail.com>
2015-01-16 22:50:12 +0000

    extracted a scheduling function

--- index.js
+++ index.js
@@ -6,38 +6,32 @@ AWS.config.update({region: 'your-region'});
 var ec2 = new AWS.EC2();
 
 module.exports.changeTag = function(timeout, oldTags, newTags, beforeHook) {
-  timeout = timeout || 60 * 60 * 1000;
-  beforeHook = beforeHook || function(cb) { cb(); };
-  
-  setTimeout(function() {
-    beforeHook(changeTag);
-  }, timeout * Math.random());
-
-  function changeTag() {
+  schedule(timeout, beforeHook, function() {
     getInstanceId(oldTags, function(id) {
       ec2.createTags({ Resources: [id], Tags: newTags }, function(err, data) {
         if (err) console.log(err);
       });
     });
-  }
+  });
 }; 
 
 module.exports.terminate = function(timeout, tags, beforeHook) {
-  timeout = timeout || 60 * 60 * 1000;
-  beforeHook = beforeHook || function(cb) { cb(); };
-  
-  setTimeout(function() {
-    beforeHook(terminate);
-  }, timeout * Math.random());
-
-  function terminate() {
+  schedule(timeout, beforeHook, function() {
     getInstanceId(tags, function(id) {
       ec2.terminateInstances({ InstanceIds: [ id ] }, function(err, data) { 
         if (err) console.log(err);
       });
     });
-  }
+  });
 };
+
+function schedule(timeout, beforeHook, action) {
+  timeout = timeout || 60 * 60 * 1000;
+  beforeHook = beforeHook || function(cb) { cb(); };
+  setTimeout(function() {
+    beforeHook(action);
+  }, timeout * Math.random());
+}
 
 function getInstanceId(tags, cb) {
   var params = {
