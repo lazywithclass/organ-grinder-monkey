c8f3af3cb406451ff4e2014b336ede632037978a
lazywithclass <montecristoh@gmail.com>
2015-01-16 22:17:22 +0000

    extracted a common function getInstanceId

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ AWS.config.update({region: 'your-region'});
 
 var ec2 = new AWS.EC2();
 
-module.exports.changeTag = function(timeout, oldTag, newTags, beforeHook) {
+module.exports.changeTag = function(timeout, oldTags, newTags, beforeHook) {
   timeout = timeout || 60 * 60 * 1000;
   beforeHook = beforeHook || function(cb) { cb(); };
   
@@ -14,33 +14,9 @@ module.exports.changeTag = function(timeout, oldTag, newTags, beforeHook) {
   }, timeout * Math.random());
 
   function changeTag() {
-    var params = {
-      Filters: [
-        { Name: 'instance-state-name', Values: ['running'] },
-        { Name: 'tag-key', Values: oldTag.names },
-        { Name: 'tag-value', Values: oldTag.values }
-      ]
-    };
-    
-    ec2.describeInstances(params, function(error, data) {
-      if (error) return console.log(error); 
-
-      var instanceIds = data.Reservations.map(function(reservation) {
-        return reservation.Instances[0].InstanceId;
-      });
-
-      var instanceId = instanceIds[Math.floor(Math.random() * instanceIds.length)];
-
-      var params = {
-        Resources: [ instanceId ],
-        Tags: newTags
-      };
-
-      ec2.createTags(params, function(err, data) {
-        if (error) console.log(err);
-        // TODO it seems that data is {}
-        // add some kind of information about what instance
-        // was retagged
+    getInstanceId(oldTags, function(id) {
+      ec2.createTags({ Resources: [id], Tags: newTags }, function(err, data) {
+        if (err) console.log(err);
       });
     });
   }
@@ -55,27 +31,29 @@ module.exports.terminate = function(timeout, tags, beforeHook) {
   }, timeout * Math.random());
 
   function terminate() {
-    var params = {
-      Filters: [
-        { Name: 'instance-state-name', Values: ['running'] },
-        { Name: 'tag-key', Values: tags.names },
-        { Name: 'tag-value', Values: tags.values }
-      ]
-    };
-    
-    ec2.describeInstances(params, function(error, data) {
-      if (error) return console.log(error); 
-
-      var instanceIds = data.Reservations.map(function(reservation) {
-        return reservation.Instances[0].InstanceId;
-      });
-
-      var instanceId = instanceIds[Math.floor(Math.random() * instanceIds.length)];
-
-      ec2.terminateInstances({ InstanceIds: [ instanceId ] }, function(err, data) { 
-        if (error) console.log(err);
-        console.log(data);
+    getInstanceId(tags, function(id) {
+      ec2.terminateInstances({ InstanceIds: [ id ] }, function(err, data) { 
+        if (err) console.log(err);
       });
     });
   }
 };
+
+function getInstanceId(tags, cb) {
+  var params = {
+    Filters: [
+      { Name: 'instance-state-name', Values: ['running'] },
+      { Name: 'tag-key', Values: tags.names },
+      { Name: 'tag-value', Values: tags.values }
+    ]
+  };
+  
+  ec2.describeInstances(params, function(error, data) {
+    if (error) return console.log(error);
+    var instanceIds = data.Reservations.map(function(reservation) {
+      return reservation.Instances[0].InstanceId;
+    });
+    var instanceId = instanceIds[Math.floor(Math.random() * instanceIds.length)];
+    cb(instanceId);
+  });
+}
